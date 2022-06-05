8e35a2d82604aa83f3cdd1b209576bdd5c0bc382
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-06-05 18:34:37 -0300

    agregado comentario de como se comprueba

--- js/ejercicios/ejercicio-5-tp3.js
+++ js/ejercicios/ejercicio-5-tp3.js
@@ -4,11 +4,14 @@ function checkType(centence) {
     centence = String(centence).trim(); // String hace manipulable la "centence" y luego trim quita los espacios en blanco
 
     //Valida si los caracteres estan en mayusculas, minusculas o los 2
+    // para entender la comprobacion ir a https://www.w3schools.com/jsref/jsref_obj_regexp.asp
     const regxs = {
         "lower": /^[a-z0-9 ]+$/,
         "upper": /^[A-Z0-9 ]+$/,
         "upperLower": /^[A-Za-z0-9 ]+$/
     }
+    // ^[] comprueba que la cadena coincida con los parametros dentro de corchetes.
+    //[]+ comprueba que la cadena coincida con al menos uno de los parametros dentro de corchetes.
 
     //Test comprueba si el elemento puesto como parametro cumple con las condiciones puestas por la validacion
     if (regxs.lower.test(centence)) return '0';
